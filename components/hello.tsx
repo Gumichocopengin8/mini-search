8b14f2751f974a2211fa7de9baeaeefb33bf95aa
Keita Nonaka <iKonnyaku40@gmail.com>
2022-03-15 01:44:18 -0700

    chore: library install and settings

---
+++ components/hello.tsx
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+
+const Hello = () => {
+  return (
+    <div>
+      <Button variant="text">Text</Button>
+    </div>
+  );
+};
+
+export default Hello;
